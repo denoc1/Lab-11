2a3a51d91ec251259cdd63be00d8cd07d3615fa7
denoc1 <176614645+denoc1@users.noreply.github.com>
2024-10-11 01:57:36 +0000

    Lab 11 template

---
+++ Lab11.java
@@ -0,0 +1,20 @@
+/* Header comments here  */
+
+public class Lab11{
+
+    //dayOfWeek method – given a date, returns the day of week
+    //precondition:  int m(month), int d (day), int y (year)
+    //postcondition:  returns the day of the week (String)
+
+   // public static String dayOfWeek(int m, int d, int y)
+   // {
+    	// fill in code and comments
+   // }// end of method dayOfWeek
+
+
+    public static void main (String[] args){
+
+            //fill in code and comments
+    }//end of main
+
+}//end of class Lab11
